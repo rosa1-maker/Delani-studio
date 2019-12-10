16e30fded396b0c927fb7717268afd33ca5189ae
rosa1-maker <teresawanjiku2000@gmail.com>
2019-12-10 09:57:56 +0300

    created functions for the hovering of the portfolio pictures

--- js/sripts.js
+++ js/sripts.js
@@ -1,25 +1,34 @@
-$(document).ready(function(){
-    $('.design-row p').hide();
-    $('.design-row').click(function(){
-        $('.design-row p').toggle();
-        $('.design-row img').toggle();
-    });
+$(document).ready(function() {
+  $(".design-row p").hide();
+  $(".design-row").click(function() {
+    $(".design-row p").toggle();
+    $(".design-row img").toggle();
+  });
 });
-$(document).ready(function(){
-    $('.card-dev p').hide();
-    $('.card-dev').click(function(){
-        $('.card-dev p').toggle();
-        $('.card-dev img').toggle();
-    });
-
+$(document).ready(function() {
+  $(".card-dev p").hide();
+  $(".card-dev").click(function() {
+    $(".card-dev p").toggle();
+    $(".card-dev img").toggle();
+  });
 });
-$(document).ready(function(){
-    $('.bottom p').hide();
-    $('.bottom').click(function(){
-        $('.bottom p').toggle();
-        $('.bottom img').toggle();
-    });
-    $('.col').hover(function(){
-      $('.text',this).slidetoggle('slow');
-    });
+$(document).ready(function() {
+  $(".bottom p").hide();
+  $(".bottom").click(function() {
+    $(".bottom p").toggle();
+    $(".bottom img").toggle();
+  });
+  $(".col").hover(function() {
+    $(".text", this).toggle("slow");
+  });
+  $(".submit").click(function() {
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
+    if (name !== "" && email !== "" && message !== "") {
+      alert("Thank you." + "Your form has been submitted successfully", +name);
+    } else {
+      alert("please fill in the forms");
+    }
+  });
 });
